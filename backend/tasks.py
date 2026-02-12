2558d8de09203b90190ec168b9bbbb028b7f2d80
Andrew Velasquez <andrewvel35@gmail.com>
2026-02-12 13:06:43 -0500

    Added log timing to tasks.py.

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,11 +1,25 @@
+import logging
 from celery import Celery
 import time
+
+# Configure loggin
+logger = logging.getLogger(__name__)
 
 # Connect to Redis
 app = Celery("tasks", broker="redis://redis:6379/0")
 
 
-@app.task
-def heavy_computation(data):
-    time.sleep(5)  # Simulate a 5-second heavy job
-    return f"Processed: {data}"
+@app.task(bind=True)
+def heavy_computation(self, data):
+    # Step 1: Initialize
+    self.update_state(
+        state="PROGRESS", meta={"current": 0, "total": 100, "phase": "Initializing"}
+    )
+    time.sleep(2)
+
+    self.update_state(
+        state="PROGRESS", meta={"current": 50, "total": 100, "phase": "Crunching Data"}
+    )
+    time.sleep(2)
+
+    return f"Completed processing {data}"
